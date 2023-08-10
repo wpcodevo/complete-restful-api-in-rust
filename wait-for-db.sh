31ad845e6391418bf30483059ecd0d65f35228da
wpcodevo <ziddah.edem@gmail.com>
2023-08-10 21:54:31 +0000

    removed the checking of the database ready

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# Function to wait for the PostgreSQL database to be ready
-wait_for_db() {
-    echo "Waiting for the database to be ready..."
-    until pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT >/dev/null 2>&1; do
-        sleep 1
-    done
-    echo "Database is ready!"
-}
-
 # Function to install sqlx-cli and apply migrations
 install_and_apply_migrations() {
     echo "Installing sqlx-cli..."
@@ -20,14 +11,9 @@ install_and_apply_migrations() {
     echo "Migrations applied!"
 }
 
-# Print the environment variables
-echo "POSTGRES_HOST: $POSTGRES_HOST"
-echo "POSTGRES_PORT: $POSTGRES_PORT"
-# Add other environment variables as needed
-
 # Determine the action based on the argument passed to the script
 if [ "$1" = "api-start" ]; then
-    wait_for_db
+    # wait_for_db
     install_and_apply_migrations
     shift
     exec "$@"
